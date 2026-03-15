7fb8d3dd809b13c9f707de26815565c782cb87f7
lesterhan <mail@lesterhan.com>
2026-03-15 13:01:19 -0400

    add delete tests

--- backend/src/routes/accounts.test.ts
+++ backend/src/routes/accounts.test.ts
@@ -30,5 +30,35 @@ describe('accounts', () => {
     expect(created.name).toBe('Cash')
     expect(created.type).toBe('cash')
     expect(created.userId).toBeDefined()
+
+    const getAccountsRes = await app.request('/api/accounts', {
+      headers: { Cookie: cookie },
+    })
+    expect(await getAccountsRes.json()).toBeArrayOfSize(1)
+  })
+
+  it('DELETE /api/accounts deletes an account', async () => {
+    const createResponse = await app.request('/api/accounts', {
+      method: 'POST',
+      headers: { Cookie: cookie, 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: 'Cash', type: 'cash', currency: 'CAD' }),
+    })
+    const getAccountsRes = await app.request('/api/accounts', {
+      headers: { Cookie: cookie },
+    })
+    expect(await getAccountsRes.json()).toBeArrayOfSize(1)
+    const created = await createResponse.json()
+
+    const res = await app.request(`/api/accounts/${created.id}`, {
+      method: 'DELETE',
+      headers: { Cookie: cookie, 'Content-Type': 'application/json' },
+    })
+    expect(res.status).toBe(204)
+
+    const getEmptyAccounts = await app.request('/api/accounts', {
+      headers: { Cookie: cookie },
+    })
+    expect(await getEmptyAccounts.json()).toBeArrayOfSize(0)
   })
 })
+
--- backend/src/routes/categories.test.ts
+++ backend/src/routes/categories.test.ts
@@ -29,5 +29,35 @@ describe('categories', () => {
     const created = await res.json()
     expect(created.name).toBe('Food')
     expect(created.userId).toBeDefined()
+
+    const getCategoriesRes = await app.request('/api/categories', {
+      headers: { Cookie: cookie },
+    })
+    expect(await getCategoriesRes.json()).toBeArrayOfSize(1)
+  })
+
+  it('DELETE /api/categories deletes an category', async () => {
+    const createResponse = await app.request('/api/categories', {
+      method: 'POST',
+      headers: { Cookie: cookie, 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: 'pants' }),
+    })
+    const getCategoriesRes = await app.request('/api/categories', {
+      headers: { Cookie: cookie },
+    })
+    expect(await getCategoriesRes.json()).toBeArrayOfSize(1)
+    const created = await createResponse.json()
+
+    const res = await app.request(`/api/categories/${created.id}`, {
+      method: 'DELETE',
+      headers: { Cookie: cookie, 'Content-Type': 'application/json' },
+    })
+    expect(res.status).toBe(204)
+
+    const getEmptyCategories = await app.request('/api/categories', {
+      headers: { Cookie: cookie },
+    })
+    expect(await getEmptyCategories.json()).toBeArrayOfSize(0)
   })
 })
+
